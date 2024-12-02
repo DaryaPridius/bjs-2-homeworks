80358db2b65bab84d9edd95fa755f8e9ba8cec82
DaryaPridius <165583752+DaryaPridius@users.noreply.github.com>
2024-12-02 18:52:07 +0300

    третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
+//Задание 1 
 function compareArrays(arr1, arr2) {
-  
+
+	return arr1.length === arr2.length && arr1.every((values, index) => values === arr2[index]);
 }
 
+//Задание 2 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+	let result = users.filter(values => values.gender === gender).map(values => values.age).reduce((acc, values, index, arr) => {
+		acc += values;
+		if (index === arr.length - 1) {
+			return acc / arr.length;
+		}
+		return acc;
+	}, 0);
+	return result;
 }
